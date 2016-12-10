2ae2e356b1e69aeb5e50d09987009c9210614c6e
ryenchen <kelihoodli@gmail.com>
2016-12-10 23:59:06 +0800

    reading batch normalizatoin's code with comments

--- src/caffe/layers/batch_norm_layer.cpp
+++ src/caffe/layers/batch_norm_layer.cpp
@@ -6,6 +6,19 @@
 
 namespace caffe {
 
+/**
+ * The definition of spatial can be found in caffe.proto:
+ * "The axis to interpret as "channels" when performing convolution.
+ *  Preceding dimensions are treated as independent inputs;
+ *  succeeding dimensions are treated as "spatial".
+ *  With (N, C, H, W) inputs, and axis == 1 (the default), we perform
+ *  N independent 2D convolutions, sliding C-channel (or (C/g)-channels, for
+ *  groups g>1) filters across the spatial axes (H, W) of the input.
+ *  With (N, C, D, H, W) inputs, and axis == 1, we perform
+ *  N independent 3D convolutions, sliding (C/g)-channels
+ *  filters across the spatial axes (D, H, W) of the input."
+ * * 
+**/
 template <typename Dtype>
 void BatchNormLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
@@ -25,9 +38,12 @@ void BatchNormLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     this->blobs_.resize(3);
     vector<int> sz;
     sz.push_back(channels_);
+    // blobs_ 0 for mean of each feature. see BatchNormLayer's hpp file for more detail description.
     this->blobs_[0].reset(new Blob<Dtype>(sz));
+    // blobs_ 1 for variance of each feature
     this->blobs_[1].reset(new Blob<Dtype>(sz));
-    sz[0] = 1;
+    sz[0]=1;
+    // blobs_ 2 for value of moving_average_fraction
     this->blobs_[2].reset(new Blob<Dtype>(sz));
     for (int i = 0; i < 3; ++i) {
       caffe_set(this->blobs_[i]->count(), Dtype(0),
@@ -36,6 +52,8 @@ void BatchNormLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
   }
   // Mask statistics from optimization by setting local learning rates
   // for mean, variance, and the bias correction to zero.
+  // As in ResNet: https://github.com/KaimingHe/deep-residual-networks/blob/master/prototxt/ResNet-152-deploy.prototxt     
+  // So it doesn't need to set three lr_mult: 0
   for (int i = 0; i < this->blobs_.size(); ++i) {
     if (this->layer_param_.param_size() == i) {
       ParamSpec* fixed_param_spec = this->layer_param_.add_param();
@@ -53,26 +71,33 @@ void BatchNormLayer<Dtype>::Reshape(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
   if (bottom[0]->num_axes() >= 1)
     CHECK_EQ(bottom[0]->shape(1), channels_);
+  // training data instances' num and features' num are the same as input from bottom_data(one blob for one batch_size)
   top[0]->ReshapeLike(*bottom[0]);
 
   vector<int> sz;
   sz.push_back(channels_);
   mean_.Reshape(sz);
   variance_.Reshape(sz);
+  // a temporary variable. (X-EX)^2 when compute variance; denominator of normalize step when do normalize 
   temp_.ReshapeLike(*bottom[0]);
+  // Batch-Normalized x. As Algorithm 1's last step.
   x_norm_.ReshapeLike(*bottom[0]);
-  sz[0] = bottom[0]->shape(0);
+  sz[0]=bottom[0]->shape(0);
+  // TODO
   batch_sum_multiplier_.Reshape(sz);
 
+  // (N * C * H * W) / (C * N) = H * W
   int spatial_dim = bottom[0]->count()/(channels_*bottom[0]->shape(0));
   if (spatial_sum_multiplier_.num_axes() == 0 ||
       spatial_sum_multiplier_.shape(0) != spatial_dim) {
     sz[0] = spatial_dim;
     spatial_sum_multiplier_.Reshape(sz);
+    // TODO what's spatial_sum_multiplier for?
     Dtype* multiplier_data = spatial_sum_multiplier_.mutable_cpu_data();
     caffe_set(spatial_sum_multiplier_.count(), Dtype(1), multiplier_data);
   }
 
+  // TODO K × N
   int numbychans = channels_*bottom[0]->shape(0);
   if (num_by_chans_.num_axes() == 0 ||
       num_by_chans_.shape(0) != numbychans) {
@@ -95,6 +120,7 @@ void BatchNormLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     caffe_copy(bottom[0]->count(), bottom_data, top_data);
   }
 
+  // During train phase, use_global_stats = false by the setting in LayerSetup
   if (use_global_stats_) {
     // use the stored mean/variance estimates.
     const Dtype scale_factor = this->blobs_[2]->cpu_data()[0] == 0 ?
@@ -104,60 +130,84 @@ void BatchNormLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
     caffe_cpu_scale(variance_.count(), scale_factor,
         this->blobs_[1]->cpu_data(), variance_.mutable_cpu_data());
   } else {
-    // compute mean
+    // compute mean. the first step(mini-batch mean) in paper's Algorithm 1.
+    // (1.0/num) * bottom_data * spatial_sum_multiplier
+    // 1         * (N × K)     * (1) -> each element in bottom_data is divided by (num * spatial_dim)
     caffe_cpu_gemv<Dtype>(CblasNoTrans, channels_ * num, spatial_dim,
         1. / (num * spatial_dim), bottom_data,
         spatial_sum_multiplier_.cpu_data(), 0.,
         num_by_chans_.mutable_cpu_data());
+    // num_by_chans^T * batch_sum_multiplier_
+    // (K × N)        * (N × 1) --> each element in bottom_data is first sum across each spatial then divided by (num * spatial_dim). As in paper's Algorithm 1. 
     caffe_cpu_gemv<Dtype>(CblasTrans, num, channels_, 1.,
         num_by_chans_.cpu_data(), batch_sum_multiplier_.cpu_data(), 0.,
         mean_.mutable_cpu_data());
   }
 
   // subtract mean
+  // For efficient computation, the substraction of mean takes two steps: 
+  // 1. batch_sum_multiplier * mean_         --> num_by_chans 
+  //    (num × 1)            * (1 × channel) --> (num × channel)
   caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, num, channels_, 1, 1,
       batch_sum_multiplier_.cpu_data(), mean_.cpu_data(), 0.,
       num_by_chans_.mutable_cpu_data());
+  // 2. num_by_chans * spatial_sum_multiplier  --> top_data
+  //    (K * N × 1)  * (1 × 1)                 --> (num * channels)
   caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, channels_ * num,
       spatial_dim, 1, -1, num_by_chans_.cpu_data(),
       spatial_sum_multiplier_.cpu_data(), 1., top_data);
 
   if (!use_global_stats_) {
-    // compute variance using var(X) = E((X-EX)^2)
+    // compute variance using var(X) = E((X-EX)^2). the second step(mini-batch variance) in paper's Algorithm 1.
     caffe_powx(top[0]->count(), top_data, Dtype(2),
-        temp_.mutable_cpu_data());  // (X-EX)^2
+        temp_.mutable_cpu_data());  				// (X-EX)^2
+
+    // temp_   * spatial_sum_multiplier --> num_by_chans
+    // (N × K) * (1)                --> (N × K).		// (X-EX)^2 -> (X-EX)^2 / (num * spatial_dim)
     caffe_cpu_gemv<Dtype>(CblasNoTrans, channels_ * num, spatial_dim,
         1. / (num * spatial_dim), temp_.cpu_data(),
         spatial_sum_multiplier_.cpu_data(), 0.,
         num_by_chans_.mutable_cpu_data());
+    // num_by_chans^T * batch_sum_multiplier --> variance
+    // (K × N)        * (N × 1)              --> (K × 1)
     caffe_cpu_gemv<Dtype>(CblasTrans, num, channels_, 1.,
         num_by_chans_.cpu_data(), batch_sum_multiplier_.cpu_data(), 0.,
-        variance_.mutable_cpu_data());  // E((X_EX)^2)
+        variance_.mutable_cpu_data());  // E((X_EX)^2)		// sum((X-EX)^2 / (num * spatial_dim)) across each channels -> E((X-EX)^2)
 
     // compute and save moving average
     this->blobs_[2]->mutable_cpu_data()[0] *= moving_average_fraction_;
     this->blobs_[2]->mutable_cpu_data()[0] += 1;
+    // mean_ = \lambda * mean_ + mean_
     caffe_cpu_axpby(mean_.count(), Dtype(1), mean_.cpu_data(),
         moving_average_fraction_, this->blobs_[0]->mutable_cpu_data());
     int m = bottom[0]->count()/channels_;
     Dtype bias_correction_factor = m > 1 ? Dtype(m)/(m-1) : 1;
+    // variance _ = \lambda * variance_ + variance_ 
     caffe_cpu_axpby(variance_.count(), bias_correction_factor,
         variance_.cpu_data(), moving_average_fraction_,
         this->blobs_[1]->mutable_cpu_data());
   }
 
-  // normalize variance
+  // normalize variance. the third step(normalize) in paper's Algorithm 1. 
+  // variance_i = variance_i + eps_
   caffe_add_scalar(variance_.count(), eps_, variance_.mutable_cpu_data());
+  // variance_i = (variance_i)^0.5
   caffe_powx(variance_.count(), variance_.cpu_data(), Dtype(0.5),
              variance_.mutable_cpu_data());
 
   // replicate variance to input size
+  // batch_sum_multiplier_ * variance_ --> num_by_chans 
+  // (N × 1)               * (K × 1)   --> (N × K)
   caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, num, channels_, 1, 1,
       batch_sum_multiplier_.cpu_data(), variance_.cpu_data(), 0.,
       num_by_chans_.mutable_cpu_data());
+  // num_by_chans_ * spatial_sum_multiplier_ --> temp_
+  // (N × K)       * (K × 1)                 --> (N × 1)
   caffe_cpu_gemm<Dtype>(CblasNoTrans, CblasNoTrans, channels_ * num,
       spatial_dim, 1, 1., num_by_chans_.cpu_data(),
       spatial_sum_multiplier_.cpu_data(), 0., temp_.mutable_cpu_data());
+  // top_data / temp_ 	--> top_data
+  // (N × K)  / (N × 1) --> (N × K)
   caffe_div(temp_.count(), top_data, temp_.cpu_data(), top_data);
   // TODO(cdoersch): The caching is only needed because later in-place layers
   //                 might clobber the data.  Can we skip this if they won't?
